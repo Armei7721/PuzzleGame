4429392fc8d3aecd406d066b576d32583e79d51e
Armei7721 <127286133+Armei7721@users.noreply.github.com>
2023-11-10 16:50:37 +0900

    20231110
    12310

---
+++ Assets/Scenes/SceneChange.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class SceneChange : MonoBehaviour
+{
+    public void GameSecenCtrl()
+    {
+        SceneManger.LoadScene("SkyPuzzle");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
