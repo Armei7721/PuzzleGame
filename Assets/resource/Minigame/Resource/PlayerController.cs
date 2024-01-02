fd1608e121da32f7d0db635055d892f104dbb4bf
Armei7721 <127286133+Armei7721@users.noreply.github.com>
2024-01-02 16:37:39 +0900

    2024102

--- Assets/resource/Minigame/Resource/PlayerController.cs
+++ Assets/resource/Minigame/Resource/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 public class PlayerController : MonoBehaviour
-{
+{   public GameObject[] childlist; 
     public bool canAttack;
     public float attackCooldown = 0.6f;
     public float special_attackCooldown = 1.5f;
@@ -11,11 +11,14 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D PRigidBody;
     public float speed = 5;
     public int jumpCount = 0;
+    public float hittime;
+
     bool isGround = true;
-    bool isDead;
+    bool isDead = false;
     bool attack = false;
-
-
+    bool baldong = false;
+    bool hitting = false;
+    bool isHit = false;
     public Tilemap tilemap;
     // Start is called before the first frame update
     void Start()
@@ -23,7 +26,7 @@ public class PlayerController : MonoBehaviour
         canAttack = true;
         animator = GetComponent<Animator>();
         PRigidBody = GetComponent<Rigidbody2D>();
-
+        Childlist();
     }
 
     // Update is called once per frame
@@ -32,8 +35,12 @@ public class PlayerController : MonoBehaviour
         StartCoroutine(Dig());
         StartCoroutine(Attack());
         StartCoroutine(Special_Attack());
-        Move();
-        Jump();
+        if (!isHit)
+        {
+            Move();
+            Jump();
+        }
+        HitDuring();
     }
     //플레이어 움직임 컨트롤
     private void Move()
@@ -87,22 +94,46 @@ public class PlayerController : MonoBehaviour
     }
 
     public IEnumerator Attack()
-    {
+    {   
         if (Input.GetKeyDown(KeyCode.Z) && canAttack)
         {
             canAttack = false;
-            animator.SetBool("Attack", true);
-            PRigidBody.velocity = new Vector2(0f,PRigidBody.velocity.y);
+            attack = true;
+            animator.SetBool("Attack", attack);
+            PRigidBody.velocity = new Vector2(0f, PRigidBody.velocity.y);
             yield return new WaitForSeconds(0.2f);
-            animator.SetBool("Attack", false);
+            attack = false; ;
+            animator.SetBool("Attack", attack);
             yield return new WaitForSeconds(attackCooldown);
 
             canAttack = true;
         }
     }
+    public void ColliderControl()
+    {
+        if(childlist[0].activeSelf)
+        {
+            childlist[0].SetActive(false);
+        }
+        else
+        {
+            childlist[0].SetActive(true);
+        }
+    }
+    public void SpecialColldier()
+    {
+        if (childlist[1].activeSelf)
+        {
+            childlist[1].SetActive(false);
+        }
+        else
+        {
+            childlist[1].SetActive(true);
+        }
+    }
     public IEnumerator Special_Attack()
     {
-        if(Input.GetKeyDown(KeyCode.X) && canAttack)
+        if (Input.GetKeyDown(KeyCode.X) && canAttack)
         {
             canAttack = false;
             animator.SetBool("Special_Attack", true);
@@ -113,7 +144,7 @@ public class PlayerController : MonoBehaviour
             canAttack = true;
         }
     }
-     public IEnumerator Dig()
+    public IEnumerator Dig()
     {
         if (Input.GetKeyDown(KeyCode.C) && canAttack && Input.GetKey(KeyCode.DownArrow))
         {
@@ -128,13 +159,53 @@ public class PlayerController : MonoBehaviour
             Debug.DrawRay(cellCenter, Vector3.up * 0.5f, Color.red);
             TileBase tile = tilemap.GetTile(playerGridPosition);
 
-            
+
             if (tile != null)
             {
                 // 타일맵에서 해당 위치의 타일을 삭제합니다.
                 tilemap.SetTile(playerGridPosition, null);
             }
 
+            yield return new WaitForSeconds(attackCooldown);
+            canAttack = true;
+        }
+        if (Input.GetKeyDown(KeyCode.C) && canAttack && (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow)))
+        {
+            
+            if (PRigidBody.velocity.x > 0)
+            {
+                Vector3Int playerGridPosition = tilemap.WorldToCell(transform.position + Vector3.right);
+                Vector3Int cellPosition = tilemap.WorldToCell(transform.position + Vector3.right);
+                Vector3 cellCenter = tilemap.GetCellCenterWorld(cellPosition);
+                TileBase tile = tilemap.GetTile(playerGridPosition);
+
+
+                if (tile != null)
+                {
+                    // 타일맵에서 해당 위치의 타일을 삭제합니다.
+                    tilemap.SetTile(playerGridPosition, null);
+                }
+            }
+            else if (PRigidBody.velocity.x < 0)
+            {
+                Vector3Int playerGridPosition = tilemap.WorldToCell(transform.position + Vector3.left);
+                Vector3Int cellPosition = tilemap.WorldToCell(transform.position + Vector3.left);
+                Vector3 cellCenter = tilemap.GetCellCenterWorld(cellPosition);
+                
+                TileBase tile = tilemap.GetTile(playerGridPosition);
+
+
+                if (tile != null)
+                {
+                    // 타일맵에서 해당 위치의 타일을 삭제합니다.
+                    tilemap.SetTile(playerGridPosition, null);
+                }
+            }
+            canAttack = false;
+            animator.SetBool("Dig", true);
+
+            yield return new WaitForSeconds(0.2f);
+            animator.SetBool("Dig", false);
             yield return new WaitForSeconds(attackCooldown);
             canAttack = true;
         }
@@ -148,25 +219,97 @@ public class PlayerController : MonoBehaviour
             return;
         }
         //Jump 버튼을 눌렀을 때 이고 isJump false 일때 발동!
-        if (Input.GetButtonDown("Jump") && jumpCount<2)
+        if (Input.GetButtonDown("Jump") && jumpCount < 2)
         {
-            int jumpForce = 5;
+            int jumpForce = 10;
             jumpCount++;
-            isGround = false;
+
             PRigidBody.velocity = Vector2.zero;
             //플레이어의 리지드 바디에  Vector2.up 방향으로 jumpForce 힘만큼 힘을 준다.
             PRigidBody.velocity = (Vector2.up * jumpForce);
+            animator.SetBool("isGround", false);
             //애니메이터 isJump는 트루!
-            animator.SetTrigger("Jump");
+            animator.SetBool("Jump", true);
         }
 
     }
+    public void HitDuring()
+    {
+        if (!isDead)
+        {
+            // 피격 상태일 때의 처리
+            if (isHit)
+            {
+                float hitDuration=1;
+                gameObject.layer = 14;
+                hitting = true;
+                animator.SetBool("hurt", hitting);
+                hittime += Time.deltaTime;
+                GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+                Vector2 knockBackDirection = -transform.right; // 넉백되는 방향 (캐릭터의 뒤쪽)
+                float knockBackForce = 2.0f;
+                if (hittime < 0.5f)
+                {
+                    PRigidBody.velocity =new Vector2(knockBackDirection.x * knockBackForce,knockBackForce);
+                    
+                }
+                if (hittime >= hitDuration)
+                {
+                    hitting = false;
+                    animator.SetBool("hurt", hitting);
+
+
+
+                    if (hittime >= hitDuration + 3f)
+                    {
+                        hitting = false;
+                        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1f);
+                        gameObject.layer = 13;
+                        isHit = false;
+                        hittime = 0;
+                    }
+
+
+                }
+
+
+                return; // 피격 상태일 때는 이동 및 공격을 하지 않음
+            }
+            isHit = false;
+        }
+    }
+    public void Childlist()
+    {
+        GameObject parentObject = GameObject.Find("Player");
+        if (parentObject != null)
+        {
+            Transform[] children = parentObject.GetComponentsInChildren<Transform>(true);
+
+            // 자식 오브젝트들만 참조하는데, 부모 자신은 제외하기 위해 배열 크기를 조정합니다.
+            childlist = new GameObject[children.Length - 1];
+
+            // 첫 번째 요소는 부모 자신이므로 인덱스를 1부터 시작하여 자식 오브젝트들을 참조합니다.
+            for (int i = 1; i < children.Length; i++)
+            {
+                childlist[i - 1] = children[i].gameObject;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("DicePlane을 찾을 수 없습니다.");
+        }
+    }
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.CompareTag("ground"))
+        if (collision.collider.CompareTag("ground"))
         {
             jumpCount = 0;
-            isGround = true;
+            animator.SetBool("Jump", false);
+            animator.SetBool("isGround", true);
+        }
+        else if(collision.collider.CompareTag("enemy"))
+        {
+            isHit = true;
         }
     }
 }
